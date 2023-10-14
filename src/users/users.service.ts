f961a7b3eccab6f948235b3798735dcfa5543f0a
Mạnh Đạt <tranhuudats1@gmail.com>
2023-10-15 06:34:35 +0700

    CRUD Users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,40 +1,46 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { InjectModel } from '@nestjs/mongoose';
-import { User } from './schemas/user.schema';
-import { Model } from 'mongoose';
+import {Injectable} from '@nestjs/common';
+import {CreateUserDto} from './dto/create-user.dto';
+import {UpdateUserDto} from './dto/update-user.dto';
+import {InjectModel} from '@nestjs/mongoose';
+import {User} from './schemas/user.schema';
+import mongoose, {Model} from 'mongoose';
 import {genSaltSync, hashSync} from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+    constructor(@InjectModel(User.name) private userModel: Model<User>) {
+    }
 
-  getHashPassword = (password: string) => {
-    const salt = genSaltSync(10);
-    const hash = hashSync(password, salt);
-    return hash; // Add this line to return the hash value
-  };
+    getHashPassword = (password: string) => {
+        const salt = genSaltSync(10);
+        const hash = hashSync(password, salt);
+        return hash; // Add this line to return the hash value
+    };
 
-  async create(createUserDto: CreateUserDto) {
-    const hashPassword = this.getHashPassword(createUserDto.password);
-    const user = await this.userModel.create({ ...createUserDto, password: hashPassword });
-    return user;
-  }
+    async create(createUserDto: CreateUserDto) {
+        const hashPassword = this.getHashPassword(createUserDto.password);
+        const user = await this.userModel.create({...createUserDto, password: hashPassword});
+        return user;
+    }
 
-  findAll() {
-    return `This action returns all users`;
-  }
+    findAll() {
+        return `This action returns all users`;
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    findOne(id: string) {
+        if (!mongoose.Types.ObjectId.isValid(id)) return "Not Found User!";
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+        return this.userModel.findOne({_id: id});
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+    }
+
+    async update(updateUserDto: UpdateUserDto) {
+        return this.userModel.updateOne({_id: updateUserDto._id}, {...updateUserDto});
+    }
+
+    remove(id: string) {
+        if (!mongoose.Types.ObjectId.isValid(id)) return "Not Found User!";
+
+        return this.userModel.deleteOne({_id: id});
+    }
 }
