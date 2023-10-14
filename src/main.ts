38e2b0ba764376d2214590bef48d11da6b07b5fa
Mạnh Đạt <tranhuudats1@gmail.com>
2023-10-14 20:56:05 +0700

    Validation

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import {NestExpressApplication} from "@nestjs/platform-express";
 import {join} from 'path';
 import * as process from "process";
 import {ConfigService} from "@nestjs/config";
+import {ValidationPipe} from "@nestjs/common";
 require('dotenv').config();
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     const configService = app.get(ConfigService);
+
+    app.useGlobalPipes(new ValidationPipe());
 
     app.useStaticAssets(join(__dirname, '..', 'public'));
     app.setBaseViewsDir(join(__dirname, '..', 'views'));
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import {IsEmail, IsNotEmpty, IsString} from "class-validator";
+
 export class CreateUserDto {
+    @IsEmail({}, {message: 'Please Write Correct Email!'})
+    @IsNotEmpty({message: 'Email Cannot Empty!'})
     email: string;
+
+    @IsNotEmpty({
+        message: 'Password Cannot Empty!',
+    })
     password: string;
+
+
     name: string;
+    address: string;
 }
